660cbed7b6c9606af7db64898f60eed605eb09d9
Jaya Prakash M <jayaprakash.manne@intl.verizon.com>
2019-11-01 15:10:28 +0530

    Adding the first commit files

---
+++ src/main/java/com/abs/oec/OECApplication.java
@@ -0,0 +1,28 @@
+/**
+* @author  Jaya Prakash Manne
+* @version 1.0
+* @since   01-Nov-2019 
+*/
+package com.abs.oec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class OECApplication {
+	private static final Logger LOGGER = LoggerFactory.getLogger(OECApplication.class);
+	
+	//=========================================================================
+	
+	public static void main(String[] args) {
+		SpringApplication.run(OECApplication.class, args);
+		//LOGGER.info("Simple log statement with inputs {}, {} and {}", 1,2,3);
+		LOGGER.info("Start of the OECApplication............... ");
+	}
+	
+	//=========================================================================
+}
---
+++ src/main/java/com/abs/oec/repository/NoteRepository.java
@@ -0,0 +1,16 @@
+/**
+* @author  Jaya Prakash Manne
+* @version 1.0
+* @since   01-Nov-2019 
+*/
+package com.abs.oec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.abs.oec.model.Note;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
+}
